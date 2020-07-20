43b8752309922f3eafef2ba6b283bfcbf3d1e73b
Syeong2 <ssssi46@naver.com>
2020-07-20 06:38:11 -0700

    Modify add-nbo.c

--- add-nbo.c
+++ add-nbo.c
@@ -5,17 +5,19 @@
 int main(int argc, char *argv[]){
 
 	FILE *fd, *fd2;
-	uint32_t sn, sn2;
+	uint32_t n, n2;
 
 	fd = fopen(argv[1], "rb");
 	fd2 = fopen(argv[2], "rb");
 
-	fread(&sn, sizeof(sn), 1, fd);
-	sn=ntohl(sn);
+	fread(&n, sizeof(n), 1, fd);
+	n=ntohl(n);
 
-	fread(&sn2, sizeof(sn2), 1, fd2);
-	sn2=ntohl(sn2);
+	fread(&n2, sizeof(n2), 1, fd2);
+	n2=ntohl(n2);
 
-	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", sn, sn, sn2, sn2, sn+sn2, sn+sn2);
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", n, n, n2, n2, n+n2, n+n2);
 
+	fclose(fd);
+	fclose(fd2);
 }
